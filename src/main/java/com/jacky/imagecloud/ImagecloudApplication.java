6b0328a6c70bb01d559cd0c0a3751b8bd28974c1
Jacky <964413011@qq.com>
2021-02-16 15:10:36 +0800

    init project

---
+++ src/main/java/com/jacky/imagecloud/ImagecloudApplication.java
@@ -0,0 +1,13 @@
+package com.jacky.imagecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagecloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacky/imagecloud/ImagecloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacky.imagecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagecloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
