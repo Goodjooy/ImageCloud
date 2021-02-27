6aa514d0319fbdbf204443622c7056fc325cbfe8
Jacky <964413011@qq.com>
2021-02-27 12:49:20 +0800

    更新readme

--- src/test/java/com/jacky/imagecloud/ImagecloudApplicationTests.java
+++ src/test/java/com/jacky/imagecloud/ImagecloudApplicationTests.java
@@ -1,7 +1,6 @@
 package com.jacky.imagecloud;
 
-import com.jacky.imagecloud.controller.SecurityController;
-import com.jacky.imagecloud.data.Result;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,15 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ImagecloudApplicationTests {
 
-	@Autowired
-	SecurityController controller;
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void emailCheck(){
-		assertEquals(controller.CheckEmailExist("12212123@qq.com").err,new Result<Boolean>(true).err);
-		assertTrue(controller.CheckEmailExist("23234").err);
-	}
+
 
 }
