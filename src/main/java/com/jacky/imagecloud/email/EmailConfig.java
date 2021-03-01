47ce45d71e53d3c5634fc669c16a19ff578bcc2f
Jacky <964413011@qq.com>
2021-03-01 15:36:31 +0800

    邮箱端口更新

--- src/main/java/com/jacky/imagecloud/email/EmailConfig.java
+++ src/main/java/com/jacky/imagecloud/email/EmailConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import javax.net.ssl.SSLSocketFactory;
+
 @ConfigurationProperties("email")
 public class EmailConfig {
     @Bean
@@ -12,7 +14,7 @@ public class EmailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
         mailSender.setHost("smtp.126.com");
-        mailSender.setPort(25);
+        mailSender.setPort(465);
 
         mailSender.setUsername("imagestorage@126.com");
         mailSender.setPassword("ARUPFZMLULNSHFOI");
@@ -20,10 +22,14 @@ public class EmailConfig {
         mailSender.setDefaultEncoding("UTF-8");
 
         var props=mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.transport.protocol", "smtps");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
+
+        props.put("mail.smtp.socketFactory.port",465);
+        props.put("mail.smtp.socketFactory.class", SSLSocketFactory.class);
+        props.put("mail.smtp.socketFactory.fallback",false);
 
         return mailSender;
     }
